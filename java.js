0a7396400e9170fce9ea94b26d15d59fac1c4426
Geez-Us <noahsullivan382@gmail.com>
2023-01-26 14:51:23 -0800

    the weather man fetches the forecast for dallas

--- java.js
+++ java.js
@@ -1,5 +1,24 @@
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + MrWeatherManKey;
-var city = input.innerHTML;
+var usercity = document.getElementById('input');
+var city = usercity.value;
 var MrWeatherManKey = "192a57d57d5d52c033c3ff8f0bae517c";
+var button = document.getElementById('go');
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + MrWeatherManKey;
 
-fetch(queryURL);
+
+
+button.addEventListener('click', function(event){
+event.preventDefault();
+fetch(queryURL).then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+})
+});
+
+/*fetch("http://api.openweathermap.org/data/2.5/weather?q=houston&appid=192a57d57d5d52c033c3ff8f0bae517c").then(function(event){
+    return event.json();
+})
+.then(function(data){
+    console.log(data);
+})*/
